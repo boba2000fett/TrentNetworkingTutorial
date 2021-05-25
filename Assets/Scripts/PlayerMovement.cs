9695bf523dccba8a303809588804b20156981842
boba2000fett <trenton.andrews2000@gmail.com>
2021-05-25 11:19:49 -0400

    Initial Unity Project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * .1f, moveVertical * .1f, 0);
+            transform.position += movement;
+
+
+        }
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+
+}
